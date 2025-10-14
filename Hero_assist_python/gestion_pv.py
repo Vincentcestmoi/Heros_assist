1e56db91ed11507e7f05aabdc024e82378c11786
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2025-10-14 19:20:33 +0200

    gestion_pv.py (fix #13) -amélioration text -contraction question -ajout poison

--- Hero_assist_python/gestion_pv.py
+++ Hero_assist_python/gestion_pv.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+from random import shuffle
+
+text_min = ["Pathétique", "Vous n'avez rien senti.", "Un bébé vous aurez frappé plus fort que ça.",
+"Vous avez presque pitié des efforts futiles de votre adversaire."]
 
 while True:
     pv_m = int(input("Nombre de PV : "))
@@ -7,8 +11,9 @@ while True:
     pv_base = pv_m
     pv = pv_m
     run = True
+    print("Lettres clé : (D)ommage | (P)oison | (S)oins | (B)oost | Boost (A)rmure | (Q)uit (=redémarre) | (R)eset")
     while run:
-        temp = input("(D)ommage | (S)oins | (B)oost | Boost (A)rmure | (Q)uit (=redémarre) | (R)eset ?")
+        temp = input("d, p, s, b, a, r, q ? ")
 
         if temp in ["S", "s"]:
             soin = int(input("Soins : "))
@@ -27,12 +32,12 @@ while True:
                 print("Vous vous trouvez toujours aux portes de la mort.")
 
         elif temp in ["B", "b"]:
-            temp2 = int(input("Boost de résistance de combien? : "))
+            temp2 = int(input("Boost de résistance de combien ? : "))
             pv_m += temp2
             pv += temp2
 
         elif temp in ["A", "a"]:
-            temp2 = int(input("Boost d'armure de combien? : "))
+            temp2 = int(input("Boost d'armure de combien ? : "))
             armure += temp2
 
         elif temp in ["Q", "q"]:
@@ -43,20 +48,29 @@ while True:
             pv = pv_base
             armure = armure_base
 
-        elif temp in ["D", "d"] :
+        elif temp in ["D", "d"]:
             attaque = int(input("Dégâts : "))
             degat = max(attaque - armure, 1)
+        elif temp in ["P", "p"]:
+            degat = int(input("Dégâts de poison : "))
+
+        if temp in ["D", "d", "P", "p"]:
             pv -= degat
             if pv <= 0:
                 print("Vous êtes mort.")
                 pv = 0
-            elif pv <= (pv_m / 10):
-                print("Votre vision se trouble, vos sens s'amenuisent, la mort vous sourit.")
-            elif degat > pv_m / 2:
-                print("Vous ressentez le choc jusque dans vos os.")
-            elif degat > pv_m / 3:
-                print("Vous sentez votre corps souffrir de l'impact.")
-            elif degat > pv_m / 4:
-                print("Vous ressentez l'attaque.")
-            else:
-                print("Vous n'avez presque rien senti.")
+            elif pv <= (pv_m * 0.8 ):
+                print("Votre vision se trouble, votre souffle s'épuise, vos sens s'amenuisent. La mort vous sourit.")
+            elif pv <= (pv_m * 0.16 ):
+                print("La douleur vous vrille le crâne, vous ne survivrez sans doute pas au prochain coup.")
+            elif degat > pv_m * 0.75:
+                print("Même vos os semblent pulser de douleur, vous luttez pour rester conscient.")
+            elif degat > pv_m * 0.6:
+                print("Votre corps tout entier vous fait souffir.")
+            elif degat > pv_m * 0.45:
+                print("Vous avez le souffle court et les muscles douloureux.")
+            elif degat > pv_m * 0.3:
+                print("Vous supportez encore plutôt bien la douleur.")
+            elif degat <= pv_m * 0.1:
+                shuffle(text_min)
+                print(text_min[0])
