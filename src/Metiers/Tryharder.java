e9e0ccf5abbf4911b2a881d1f68e687939548a17
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2025-10-24 14:37:56 +0200

    Tryharder.java -mise en place niveau tryharder (fix #45)

--- src/Metiers/Tryharder.java
+++ src/Metiers/Tryharder.java
@@ -9,23 +9,86 @@ public class Tryharder extends Joueur {
 
     public Tryharder(String nom, Position position, int ob_f, Dieux parent, int xp) {
         super(nom, position, ob_f, parent, xp);
-        vie = 5;
+        vie = 3;
         attaque = 1;
         PP = "mana";
-        PP_value = 6;
-        PP_max = 6;
+        PP_value = 2;
+        PP_max = 2;
         caracteristique = "Déterminé";
         competences = "Aucune";
         SetEffetParent();
     }
 
     @Override
+    protected void actualiser_niveau() {
+        this.vie += this.niveau;
+        this.attaque += (this.niveau + 1) / 3;
+        this.PP_max += this.niveau / 3;
+        if(this.niveau >= 5){
+            this.caracteristique += ", Doué";
+        }
+        if(this.niveau >= 8){
+            this.caracteristique += ", Talentieux";
+        }
+    }
+
+    @Override
     protected void presente_caracteristique(){
-        System.out.println("Déterminé : Gagne (très lentement) des statistiques avec la pratique."); //TODO
+        System.out.println("Déterminé : Gagne des statistiques avec la pratique.");
+        if(this.niveau >= 5){
+            System.out.println("Doué : Gagne plus rapidement de l'expérience.");
+        }
+        if(this.niveau >= 8){
+            System.out.println("Talentueux : Gagne encore plus rapidement de l'expérience.");
+        }
     }
 
     @Override
     protected void presente_pouvoir(){
+    }
+
+    @Override
+    //le tryhardeur progresse 20% plus vite (palier par 4 au lieu de 5).
+    public void gagneXp(){
+        gagneXpLocal();
+        if(this.niveau >= 5 && rand.nextInt(10) == 0){ // +10%
+            gagneXpLocal();
+        }
+        if(this.niveau >= 8 && rand.nextInt(15) <= 1){ // +13%
+            gagneXpLocal();
+        }
+        if(getXplocal() >= (this.niveau + 1) * 4){
+            int dif = getXplocal() - this.niveau * 4;
+            this.niveau += 1;
+            resetXpLocal();
+            for(int i = 0; i < dif; i++){
+                gagneXpLocal();
+            }
+            System.out.println(nom + " a gagné un niveau !");
+            lvl_up();
+        }
+    }
+
+    @Override
+    void lvl_up() {
+        this.vie += 1;
+        System.out.println("Votre résistance a légèrement augmenté.");
+        if(this.niveau % 3 == 2){
+            this.attaque += 1;
+            System.out.println("Votre attaque a légèrement augmenté.");
+        }
+        if(this.niveau % 3 == 0){
+            this.PP_max += 1;
+            System.out.println("Votre réserve de mana a légèrement augmentée.");
+        }
+        if(this.niveau == 5){
+            this.caracteristique += ", Doué";
+            System.out.println("Nouvelle capacité débloquée !");
+        }
+        if(this.niveau == 8){
+            this.caracteristique += ", Talentieux";
+            System.out.println("Nouvelle capacité débloquée !");
+        }
     }
 
     public Metier getMetier() {
