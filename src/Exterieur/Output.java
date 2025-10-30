f2f7a508f34c79205ad2ef892e48cc4487f5624f
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2025-10-30 01:18:28 +0100

    Output.java -ajout docs -factorisation dans fonction recuperation

--- src/Exterieur/Output.java
+++ src/Exterieur/Output.java
@@ -17,6 +17,11 @@ import java.util.LinkedHashSet;
 
 public class Output {
 
+    /**
+     * Met à jour le json pour enregistrer la suppression des monstres nommés
+     *
+     * @param nom la race à dismiss
+     */
     public static void dismiss_race(String nom) {
         try {
             File infoFile = new File("Save" + Main.Path + "/info.json");
@@ -68,6 +73,7 @@ public class Output {
 
     /**
      * Met à jour le json pour enregistrer la suppression des items uniques
+     *
      * @param item l'objet a dismiss
      */
     public static void dismiss_item(Pre_Equipement item) {
@@ -90,20 +96,7 @@ public class Output {
 
             if (item.rang != Rang.PROMOTION) {
                 String rangKey = item.rang.name();
-                JsonArray existants = items.containsKey(rangKey)
-                        ? items.getJsonArray(rangKey)
-                        : Json.createArrayBuilder().build();
-
-                Set<String> noms = new LinkedHashSet<>();
-                for (JsonValue val : existants) {
-                    noms.add(((JsonString) val).getString());
-                }
-                noms.add(item.nom);
-
-                JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
-                for (String nom : noms) arrayBuilder.add(nom);
-
-                itemsBuilder.add(rangKey, arrayBuilder);
+                recuperation(item, items, itemsBuilder, rangKey);
 
             } else {
                 JsonObject promoObj = items.containsKey("PROMOTION")
@@ -118,20 +111,7 @@ public class Output {
                 }
 
                 String promoKey = item.promo_type.name();
-                JsonArray existants = promoObj.containsKey(promoKey)
-                        ? promoObj.getJsonArray(promoKey)
-                        : Json.createArrayBuilder().build();
-
-                Set<String> noms = new LinkedHashSet<>();
-                for (JsonValue val : existants) {
-                    noms.add(((JsonString) val).getString());
-                }
-                noms.add(item.nom);
-
-                JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
-                for (String nom : noms) arrayBuilder.add(nom);
-
-                promoBuilder.add(promoKey, arrayBuilder);
+                recuperation(item, promoObj, promoBuilder, promoKey);
                 itemsBuilder.add("PROMOTION", promoBuilder.build());
             }
 
@@ -151,6 +131,36 @@ public class Output {
         } catch (IOException e) {
             System.err.println("Erreur lors de l'ajout d'un item unique : " + e.getMessage());
         }
+    }
+
+    /**
+     * Ajoute le nom d'un pré-équipment à une liste JSON associée à une clé promotionnelle.
+     * <p>
+     * Cette méthode vérifie si la clé {@code promoKey} existe déjà dans l'objet {@code promoObj}.
+     * Si oui, elle récupère le tableau JSON existant, sinon elle initialise un tableau vide.
+     * Elle ajoute ensuite le nom de l'objet {@code item} à l'ensemble des noms (sans doublons),
+     * puis reconstruit le tableau JSON et l'ajoute au {@code promoBuilder}.
+     *
+     * @param item        le pré-équipment à ajouter (son nom sera inséré dans le tableau)
+     * @param promoObj    l'objet JSON source contenant éventuellement des entrées existantes
+     * @param promoBuilder le constructeur JSON dans lequel le tableau mis à jour sera inséré
+     * @param promoKey    la clé sous laquelle le tableau des noms est stocké dans le JSON
+     */
+    private static void recuperation(Pre_Equipement item, JsonObject promoObj, JsonObjectBuilder promoBuilder, String promoKey) {
+        JsonArray existants = promoObj.containsKey(promoKey)
+                ? promoObj.getJsonArray(promoKey)
+                : Json.createArrayBuilder().build();
+
+        Set<String> noms = new LinkedHashSet<>();
+        for (JsonValue val : existants) {
+            noms.add(((JsonString) val).getString());
+        }
+        noms.add(item.nom);
+
+        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+        for (String nom : noms) arrayBuilder.add(nom);
+
+        promoBuilder.add(promoKey, arrayBuilder);
     }
 
     /**
@@ -180,6 +190,4 @@ public class Output {
             System.out.println("Fichier supprimé : " + f.getName());
         }
     }
-
-
 }
